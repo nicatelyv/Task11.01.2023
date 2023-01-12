1aa3dc035ecc371095fd7e2779cb1b02421b9efb
Nicat <tu72k7cwp@code.edu.az>
2023-01-12 23:20:06 +0400

    Added design

--- src/components/CounterController.js
+++ src/components/CounterController.js
@@ -1,19 +1,22 @@
 import React, { useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { bolme, decrement, increment, vurma } from '../Store/counterSlice'
-
 function CounterController() {
     const inp1 = useRef()
     const inp2 = useRef()
     const dispatch = useDispatch()
     return (
-        <div>
-            <input ref={inp1} type={"number"} />
-            <input ref={inp2} type={"number"} />
-            <button onClick={() => { dispatch(increment({ inp1: inp1.current.value, inp2: inp2.current.value })) }}>+</button>
-            <button onClick={() => { dispatch(decrement({ inp1: inp1.current.value, inp2: inp2.current.value }))}}>-</button>
-            <button onClick={() => { dispatch(vurma({ inp1: inp1.current.value, inp2: inp2.current.value }))}}>*</button>
-            <button onClick={() => { dispatch(bolme({ inp1: inp1.current.value, inp2: inp2.current.value }))}}>/</button>
+        <div className='main'>
+            <div className='inps'>
+                <input ref={inp1} type={"number"} />
+                <input ref={inp2} type={"number"} />
+            </div>
+            <div className='buttons'>
+                <button onClick={() => { dispatch(increment({ inp1: inp1.current.value, inp2: inp2.current.value })) }}>+</button>
+                <button onClick={() => { dispatch(decrement({ inp1: inp1.current.value, inp2: inp2.current.value })) }}>-</button>
+                <button onClick={() => { dispatch(vurma({ inp1: inp1.current.value, inp2: inp2.current.value })) }}>*</button>
+                <button onClick={() => { dispatch(bolme({ inp1: inp1.current.value, inp2: inp2.current.value })) }}>/</button>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './Store/store';
-
+import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <div>
+    <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
+  </div>
 );
